6a229cc1d3a7e9b8e50e3dea62595ce950fac633
Hannah Brooke <Hannah@Hannahs-MacBook-Pro.local>
2026-02-17 13:05:36 +0000

    Change to an MoE model

--- src/model.py
+++ src/model.py
@@ -1,12 +1,12 @@
+import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-model_name = "Qwen/Qwen2.5-0.5B-Instruct"
+model_name = "Qwen/Qwen1.5-MoE-A2.7B"
 
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
-    torch_dtype="auto",
-    device_map="auto",
-    local_files_only=True
+    torch_dtype=torch.bfloat16,
+    attn_implementation="sdpa"
 )
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
