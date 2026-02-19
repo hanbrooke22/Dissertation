629a90b3c2d40d1ceb7696e0cac7e557f1cac94e
Hannah Brooke [sc23heb] <sc23heb@uol-pc-055401.leeds.ac.uk>
2026-02-19 11:40:06 +0000

    Model downloaded and results run for normal routing

--- src/model.py
+++ src/model.py
@@ -1,14 +1,15 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-model_name = "mlx-community/Qwen1.5-MoE-A2.7B-4bit"
+model_name = "Qwen/Qwen1.5-MoE-A2.7B-Chat"
 
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
     device_map="auto",
-    dtype=torch.bfloat16,
+    torch_dtype=torch.bfloat16,
     attn_implementation="sdpa",
-    trust_remote_code=True
+    trust_remote_code=True,
+    load_in_4bit=True
 )
 
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
--- src/normal.py
+++ src/normal.py
@@ -1,4 +1,4 @@
-import json, torch, time
+import json, torch
 from model import model, tokenizer
 
 prompts_path = "data/prompts.jsonl"
@@ -13,15 +13,26 @@ with open(prompts_path, "r", encoding="utf-8") as f_in, open(normal_out_path, "w
         item = json.loads(line)
         prompt = item["prompt"]
 
-        model_inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt}
+        ]
+
+        text = tokenizer.apply_chat_template(
+            messages, 
+            tokenize=False,
+            add_generation_prompt=True
+        )
+
+        model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
         input_len = model_inputs.input_ids.shape[1]
 
         with torch.inference_mode():
             generated_ids = model.generate(
                 **model_inputs,
-                max_new_tokens=128,
+                max_new_tokens=512,
                 do_sample=True, 
-                num_return_sequences=5,
+                num_return_sequences=10,
                 temperature=0.8, 
                 top_p=0.95
             )
